9b4c05f7894ca128615528809864c451370ea597
XuanThuong01 <phamxuanthuong2301@gmail.com>
2022-01-18 10:30:50 +0700

    add rest password

--- course/urls.py
+++ course/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path,include,re_path
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
 from .views import CourseView, IndexView,CategoryDetailView,CourseAPI,CourseIntroduceView, LearningView
 from user.views import UserLoginView,UserRegisterView,UserLogoutView,UserChangePass
 app_name = 'course'
@@ -20,6 +22,36 @@ urlpatterns = [
     path('register', UserRegisterView.as_view(),name='register'),
     path('logout', UserLogoutView.as_view(),name='logout'),
     path('change-password', UserChangePass.as_view(),name='change_pass'),
+
+    path('password_reset/',
+        PasswordResetView.as_view(
+            template_name='user/reset_password.html',
+            # success_url = reverse_lazy('course:password_reset_done')
+            success_url=reverse_lazy('course:password_reset_done'),
+            email_template_name = 'user/password_reset_email.html',
+            subject_template_name = 'user/password_reset_subject.txt'
+
+    ),
+        name="password_reset"),
+
+    path('password_reset/done',
+        PasswordResetDoneView.as_view(
+            template_name='user/reset_password_done.html'
+    ),
+        name="password_reset_done"),
+    
+    path('password_reset_confirm/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name='user/reset_password_confirm.html',
+            success_url=reverse_lazy('course:password_reset_complete')
+    ),
+        name="password_reset_confirm"),
+
+     path('password_reset/completed/',
+        PasswordResetCompleteView.as_view(
+            template_name='user/reset_password_completed.html'
+    ),
+        name="password_reset_complete"),
 
 
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
